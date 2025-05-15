78672340366596c0361d4b46e4ca0280d05a7293
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-15 16:08:33 +0800

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -13,44 +13,9 @@ from step1x3d_geometry.models.pipelines.pipeline import Step1X3DGeometryPipeline
 
 
 
-def geometry_pipeline(input_image_path, save_glb_path):
-    """
-    The base geometry model, input image generate glb
-    """
-    pipeline = Step1X3DGeometryPipeline.from_pretrained(
-        "stepfun-ai/Step1X-3D", subfolder='Step1X-3D-Geometry-1300m'
-    ).to("cuda")
-
-    generator = torch.Generator(device=pipeline.device)
-    generator.manual_seed(2025)
-    out = pipeline(input_image_path, guidance_scale=7.5, num_inference_steps=50, generator=generator)
-
-    os.makedirs(os.path.dirname(save_glb_path), exist_ok=True)
-    out.mesh[0].export(save_glb_path)
 
 
-def geometry_label_pipeline(input_image_path, save_glb_path):
-    """
-    The label geometry model, support using label to control generation, input image generate glb
-    """
-    pipeline = Step1X3DGeometryPipeline.from_pretrained(
-        "stepfun-ai/Step1X-3D", subfolder='Step1X-3D-Geometry-Label-1300m'
-    ).to("cuda")
-    generator = torch.Generator(device=pipeline.device)
-    generator.manual_seed(2025)
 
-    out = pipeline(
-        input_image_path,
-        label={"symmetry": "x", "edge_type": "sharp"},
-        guidance_scale=7.5,
-        octree_resolution=384,
-        max_facenum=400000,
-        num_inference_steps=50,
-        generator=generator
-    )
-
-    os.makedirs(os.path.dirname(save_glb_path), exist_ok=True)
-    out.mesh[0].export(save_glb_path)
 
 
 def texture_pipeline(input_image_path, input_glb_path, save_glb_path):
@@ -66,4 +31,155 @@ def texture_pipeline(input_image_path, input_glb_path, save_glb_path):
     textured_mesh.export(save_glb_path)
 
 
+class LoadStep1X3DGeometryModel:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model_path": ("STRING", {"default": "stepfun-ai/Step1X-3D/Step1X-3D-Geometry-1300m"}),
+            }
+        }
 
+    RETURN_TYPES = ("MODEL",)
+    RETURN_NAMES = ("geometry_model",)
+    FUNCTION = "load_model"
+    CATEGORY = "Step1X-3D"
+
+    def load_model(self, model_path):
+        geometry_model = model_path
+        return (geometry_model,)
+
+
+class LoadStep1X3DGeometryLabelModel:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model_path": ("STRING", {"default": "stepfun-ai/Step1X-3D/Step1X-3D-Geometry-Label-1300m"}),
+            }
+        }
+
+    RETURN_TYPES = ("MODEL",)
+    RETURN_NAMES = ("geometry_label_model",)
+    FUNCTION = "load_model"
+    CATEGORY = "Step1X-3D"
+
+    def load_model(self, model_path):
+        geometry_label_model = model_path
+        return (geometry_label_model,)
+
+
+class LoadStep1X3DTextureModel:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model_path": ("STRING", {"default": "stepfun-ai/Step1X-3D/Step1X-3D-Texture"}),
+            }
+        }
+
+    RETURN_TYPES = ("MODEL",)
+    RETURN_NAMES = ("texture_model",)
+    FUNCTION = "load_model"
+    CATEGORY = "Step1X-3D"
+
+    def load_model(self, model_path):
+        texture_model = model_path
+        return (texture_model,)
+
+
+class LoadInputImage:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image_path": ("STRING", {"default": "examples/images/000.png"}),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("input_image_path",)
+    FUNCTION = "load_image"
+    CATEGORY = "Step1X-3D"
+
+    def load_image(self, image_path):
+        input_image_path = image_path
+        return (input_image_path,)
+
+
+class 3DGeometryGeneration:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "geometry_model": ("MODEL",),
+                "input_image_path": ("IMAGE",),
+                "guidance_scale": ("FLOAT", {"default": 7.5}),
+                "num_inference_steps": ("INT", {"default": 50}),
+                "seed": ("INT", {"default": 2025}),
+            }
+        }
+
+    RETURN_TYPES = ("UNTEXTUREDMESH",)
+    RETURN_NAMES = ("untextured_mesh",)
+    FUNCTION = "geometry_generation"
+    CATEGORY = "Step1X-3D"
+
+    def geometry_generation(self, geometry_model, input_image_path, guidance_scale, num_inference_steps, seed):
+        """
+        The base geometry model, input image generate glb
+        """
+        pipeline = Step1X3DGeometryPipeline.from_pretrained(geometry_model).to("cuda")
+    
+        generator = torch.Generator(device=pipeline.device)
+        generator.manual_seed(seed)
+        untextured_mesh = pipeline(input_image_path, guidance_scale=guidance_scale, num_inference_steps=num_inference_steps, generator=generator)
+    
+        return (untextured_mesh,)
+
+
+class 3DGeometryLabelGeneration:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "geometry_label_model": ("MODEL",),
+                "input_image_path": ("IMAGE",),
+                "symmetry": ("STRING", {"default": "x"}),
+                "edge_type": ("STRING", {"default": "sharp"}),
+                "guidance_scale": ("FLOAT", {"default": 7.5}),
+                "octree_resolution": ("INT", {"default": 384}),
+                "max_facenum": ("INT", {"default": 400000}),
+                "num_inference_steps": ("INT", {"default": 50}),
+                "seed": ("INT", {"default": 2025}),
+            }
+        }
+
+    RETURN_TYPES = ("LABELUNTEXTUREDMESH",)
+    RETURN_NAMES = ("label_untextured_mesh",)
+    FUNCTION = "geometry_label_generation"
+    CATEGORY = "Step1X-3D"
+
+    def geometry_label_generation(self, geometry_label_model, input_image_path, symmetry, edge_type, guidance_scale, octree_resolution, max_facenum, num_inference_steps, seed):
+        """
+        The label geometry model, support using label to control generation, input image generate glb
+        """
+        pipeline = Step1X3DGeometryPipeline.from_pretrained(geometry_label_model).to("cuda")
+        generator = torch.Generator(device=pipeline.device)
+        generator.manual_seed(seed)
+
+        label = {"symmetry": symmetry, "edge_type": edge_type}
+    
+        label_untextured_mesh = pipeline(
+            input_image_path,
+            label=label,
+            guidance_scale=guidance_scale,
+            octree_resolution=octree_resolution,
+            max_facenum=max_facenum,
+            num_inference_steps=num_inference_steps,
+            generator=generator
+        )
+    
+        return (label_untextured_mesh,)
+
+    
